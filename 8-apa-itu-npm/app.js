d222e464f43d6920f1f419ea8ae3d7482bcb7076
tegardm <rikutaru98@gmail.com>
2023-08-06 16:45:29 +0700

    first commit

---
+++ 8-apa-itu-npm/app.js
@@ -0,0 +1,9 @@
+const chalk = require('chalk')
+const validator = require('validator')
+
+
+console.log(chalk.italic.bgRed.yellow("Hello World") + chalk.italic.yellow(" You Are My ")+chalk.red("Shunsine.."))
+console.log(chalk`Hello Nama Saya {red.bgBlack Deyustian Muslim}`)
+
+console.log(chalk.bgGreen.italic("awoks"))
+console.log(3+3)
